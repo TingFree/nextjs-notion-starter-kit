f177a5631ecaa8a26f8d49e2efe9950246e7dbb2
Jay Ting <blueghost.contact@foxmail.com>
2023-05-26 10:55:38 +0800

    Update site.config.ts
    update rootNotionPageID

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'https://jjding.notion.site/33a8bfb62eab4d9f9fc8b0d81a1f24dc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
