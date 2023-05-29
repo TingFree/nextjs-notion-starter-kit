b0c8715ee5d319424d08c472311723882b45da41
Jay Ting <blueghost.contact@foxmail.com>
2023-05-29 20:43:04 +0800

    Update site.config.ts
    update Arsenal-AI config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'https://jjding.notion.site/Teamspace-Home-472049eaa71848f0852d0cedd0ee880a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Arsenal-AI',
+  domain: 'arsenal-ai.cn',
+  author: 'Jay Ting',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Explore AI Application',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+//   twitter: 'transitive_bs',
+  github: 'TingFree',
+  zhihu: 'Ting2021',
+//   linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
